b4e4df58e7dd096d15a8ce80c9c550eda34b5237
JhonPool4 <jhoncharaja@gmail.com>
2022-02-21 11:26:02 -0300

    inverse_kinematics_pose with max_iter input argument

--- labpythonlib/lab_functions.py
+++ labpythonlib/lab_functions.py
@@ -610,7 +610,7 @@ class Robot(object):
                 q_best = copy(q) 
         return q_best 
 
-    def inverse_kinematics_pose(self, x_des, R_des, q0):
+    def inverse_kinematics_pose(self, x_des, R_des, q0, max_iter=10):
         """
         @info: computes joint position (q) from cartesian position (xyz) and orientation(axis/angle) 
                using the method of damped pseudo-inverse.
@@ -624,7 +624,6 @@ class Robot(object):
             - q_best  : joint position
         """         
         best_norm_e     = 1e-6 
-        max_iter        = 10
         delta           = 1
         lambda_         = 0.0000001
         q               = copy(q0)
